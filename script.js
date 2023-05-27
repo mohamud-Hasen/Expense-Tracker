29c6f14663c14947ea145e5298d71bdf9dcb43a3
Mohamud E Hasen <mohmudes4@gmail.com>
2023-05-27 12:44:39 -0500

    added more js functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance');
-const moneyPlus = document.getElementById('money-plus');
-const moneyMinus = document.getElementById('money-minus');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -24,9 +24,27 @@ function addTransactionDom(transaction) {
     </span>`;
     list.appendChild(item);
 }
+
+// update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount );
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts
+     .filter(item => item > 0)
+     .reduce((acc, item) => (acc += item), 0)
+     .toFixed(2);
+     const expense = (amounts.filter(item => item < 0)
+     .reduce((acc, item) => (acc += item), 0) * -1)
+     .toFixed(2);
+     balance.innerHTML = `${total}`
+     money_plus.innerHTML = `${income}`;
+     money_minus.innerHTML = `${expense}`;
+}
 //init app
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDom);
+    updateValues();
 }
+
 init();
